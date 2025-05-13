43162d0c1bacced5ac7aeaaac25dd7c3d781920d
sonu.panchal <sonu.panchal@terranxt.com>
2025-05-13 07:18:02 +0530

    removing hardcoded API key from ocelot.json for security reasons. now setting it thorugh delegating handlers

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,8 +1,10 @@
+using ApiGateway;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddTransient<ApiKeyDelegatingHandler>();
 builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
